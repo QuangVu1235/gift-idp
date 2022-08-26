799266d19114bd298fcfd100983dc802d6cd6716
Pham Quang Vu <87456448+QuangVu1235@users.noreply.github.com>
2022-08-26 09:08:43 +0700

    ..

--- idp_gift_app/lib/src/themes/dialogs/loading_dialog.dart
+++ idp_gift_app/lib/src/themes/dialogs/loading_dialog.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:idp_gift_app/src/themes/space_values.dart';
 import 'package:idp_gift_app/src/themes/ui_colors.dart';
+import 'package:idp_gift_app/src/utils/LoadingWidget.dart';
 
 class LoadingDialog extends StatelessWidget {
   final double? elevation;
@@ -16,29 +17,47 @@ class LoadingDialog extends StatelessWidget {
     this.width,
     this.insetPadding,
     this.backgroundColor,
-    this.message = 'Vui lòng chờ...',
+    this.message = '',
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context)
-  => AlertDialog(
-    backgroundColor: backgroundColor,
-    insetPadding: insetPadding ?? const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0),
-    title: SizedBox(
-      height: height,
-      width: width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          const CircularProgressIndicator(
-            color: UIColors.title70,
-            backgroundColor: UIColors.border10,
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: UIColors.black10,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (message.isEmpty)
+                const StaggeredDotsWave(size: 45, color: UIColors.white),
+              if (message.isNotEmpty)
+                const StaggeredDotsWave(size: 45, color: UIColors.brandA),
+              if (message.isNotEmpty)
+                Text(
+                  message,
+                  style: const TextStyle(fontSize: 16, color: UIColors.brandA),
+                  textAlign: TextAlign.center,
+                ),
+            ],
           ),
-          if(message.isNotEmpty)
-            Text(message, style: const TextStyle(fontSize: 16, color: UIColors.brandA), textAlign: TextAlign.center,),
-        ],
-      ),
-    ),
-    elevation: elevation,
-  );
+        ),
+      );
+  // => AlertDialog(
+  //   backgroundColor: backgroundColor,
+  //   insetPadding: insetPadding ?? const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0),
+  //   title: SizedBox(
+  //     height: height,
+  //     width: width,
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //       children: [
+  //         // if(message.isEmpty)
+  //         //   const StaggeredDotsWave(size: 25, color: UIColors.white),
+  //         const StaggeredDotsWave(size: 25, color: UIColors.brandA),
+  //         if(message.isNotEmpty)
+  //           Text(message, style: const TextStyle(fontSize: 16, color: UIColors.brandA), textAlign: TextAlign.center,),
+  //       ],
+  //     ),
+  //   ),
+  //   elevation: elevation,
+  // );
 }
